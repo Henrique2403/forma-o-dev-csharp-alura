afb0e6a32c497b22551c7eb98cc4be5040c95e16
Henrique2403 <henriquecc6245@gmail.com>
2024-11-18 01:43:13 -0300

    Aula 1

---
+++ ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,30 @@
+﻿
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano. ");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+
+    public void ExibirTituloArtista()
+    {
+        Console.WriteLine($"Nome da música: {nome}");
+        Console.WriteLine($"Nome do artista: {artista}");
+    }
+}
+
